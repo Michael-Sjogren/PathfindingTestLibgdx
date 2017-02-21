35530fe5bdb92bfa01ff3fe3743d1c3dc5b95510
michael sjögren <mikel-carl@hotmail.com>
2017-02-21 10:25:50 +0100

    made

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -20,12 +20,13 @@ public class MyGdxGame extends ApplicationAdapter {
 	private ArrayList<Tile> openList= new ArrayList<>();
 	private ArrayList<Tile> closedList= new ArrayList<>();
 
-    public static ArrayList<Tile> allTiles = new ArrayList<>();
+    public static Tile[][] allTiles;
 
 	@Override
 	public void create () {
 		batch = new ShapeRenderer();
         batch.setAutoShapeType(true);
+        allTiles = new Tile[Gdx.graphics.getWidth() / Tile.TILE_WIDTH][Gdx.graphics.getHeight() / Tile.TILE_HEIGHT];
         addNodesToList();
         createWalls();
         setStart();
@@ -35,34 +36,40 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	public void setStart(){
         Random random = new Random();
-        int num = random.nextInt(allTiles.size());
-        while(allTiles.get(num).isEnd() && !allTiles.get(num).isWalkable()){
-            num = random.nextInt(allTiles.size());
+        int randRow = random.nextInt(Gdx.graphics.getWidth() / Tile.TILE_WIDTH);
+        int randCol = random.nextInt(Gdx.graphics.getHeight() / Tile.TILE_HEIGHT);
+        Tile tile = allTiles[randRow][randCol];
+        while(tile.isEnd() && !tile.isWalkable()){
+            randRow = random.nextInt(Gdx.graphics.getWidth() / Tile.TILE_WIDTH);
+            randCol = random.nextInt(Gdx.graphics.getHeight() / Tile.TILE_HEIGHT);
         }
-        System.out.println("START CELL:" + num );
-        allTiles.get(num).setStart(true);
+        tile = allTiles[randRow][randCol];
+        tile.setStart(true);
     }
 
     public void setEnd(){
 	    Random random = new Random();
-	    int num = random.nextInt(allTiles.size());
-        while(allTiles.get(num).isStart() && !allTiles.get(num).isWalkable()){
-            num = random.nextInt(allTiles.size());
+        int randRow = random.nextInt(Gdx.graphics.getWidth() / Tile.TILE_WIDTH);
+        int randCol = random.nextInt(Gdx.graphics.getHeight() / Tile.TILE_HEIGHT);
+        Tile tile = allTiles[randRow][randCol];
+        while(tile.isStart() && !tile.isWalkable()){
+            randRow = random.nextInt(Gdx.graphics.getWidth() / Tile.TILE_WIDTH);
+            randCol = random.nextInt(Gdx.graphics.getHeight() / Tile.TILE_HEIGHT);
         }
-        System.out.println("END CELL:" + num );
-        allTiles.get(num).setEnd(true);
+        tile = allTiles[randRow][randCol];
+        tile.setEnd(true);
     }
 
     public void createWalls(){
         for (int i = 0; i < 15; i++) {
-            allTiles.get(200 + i).setWalkable(false);
+           allTiles[5 + i][5].setWalkable(false);
         }
     }
 
 	public void addNodesToList(){
 		for (int row = 0; row < Gdx.graphics.getWidth() / 32; row++ ){
 			for (int col = 0; col < Gdx.graphics.getHeight() / 32; col++) {
-                allTiles.add(new Tile(row * Tile.TILE_WIDTH, col* Tile.TILE_HEIGHT ));
+                allTiles[row][col] = new Tile(row * Tile.TILE_WIDTH, col* Tile.TILE_HEIGHT );
 			}
 		}
 	}
@@ -75,24 +82,28 @@ public class MyGdxGame extends ApplicationAdapter {
         batch.begin();
         batch.set(ShapeRenderer.ShapeType.Line);
         batch.line(0,Gdx.graphics.getHeight() , 0 ,Gdx.graphics.getWidth() , Gdx.graphics.getHeight() , 0);
-		for (Tile tile : allTiles){
-		    if (tile.isStart()){
-                batch.set(ShapeRenderer.ShapeType.Filled);
-                batch.setColor(0,1f,0,1f);
-                batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
-            }else if(tile.isEnd()){
-                batch.set(ShapeRenderer.ShapeType.Filled);
-                batch.setColor(1f,0f,0,1f);
-                batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
-            }else if( !tile.isWalkable()){
-                batch.set(ShapeRenderer.ShapeType.Filled);
-                batch.setColor(0f,0f,1f,1f);
-                batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
-            }
-            else {
-                batch.set(ShapeRenderer.ShapeType.Line);
-                batch.setColor(1f,1f,0,1f);
-                batch.rect(tile.getX() , tile.getY() , Tile.TILE_HEIGHT , Tile.TILE_HEIGHT );
+		for (int row = 0; row < Gdx.graphics.getWidth() / Tile.TILE_WIDTH; row++){
+		    for (int col = 0; col < Gdx.graphics.getHeight() / Tile.TILE_HEIGHT; col++){
+		        Tile tile;
+                tile = allTiles[row][col];
+                if (tile.isStart()){
+                    batch.set(ShapeRenderer.ShapeType.Filled);
+                    batch.setColor(0,1f,0,1f);
+                    batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
+                }else if(tile.isEnd()){
+                    batch.set(ShapeRenderer.ShapeType.Filled);
+                    batch.setColor(1f,0f,0,1f);
+                    batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
+                }else if( !tile.isWalkable()){
+                    batch.set(ShapeRenderer.ShapeType.Filled);
+                    batch.setColor(0f,0f,1f,1f);
+                    batch.rect(tile.getX() , tile.getY() , Tile.TILE_WIDTH , Tile.TILE_HEIGHT );
+                }
+                else {
+                    batch.set(ShapeRenderer.ShapeType.Line);
+                    batch.setColor(1f,1f,0,1f);
+                    batch.rect(tile.getX() , tile.getY() , Tile.TILE_HEIGHT , Tile.TILE_HEIGHT );
+                }
             }
 
 
